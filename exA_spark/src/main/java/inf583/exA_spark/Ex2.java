262c3a3a328962e041795d0c2188d2cafb16ac53
Adam Chader <adamchader0@gmail.com>
2022-02-18 16:56:09 +0100

    ex2

--- exA_spark/src/main/java/inf583/exA_spark/Ex2.java
+++ exA_spark/src/main/java/inf583/exA_spark/Ex2.java
@@ -11,7 +11,7 @@ public class Ex2
     public static void main( String[] args )
     {
     	String inputFile = "integers.txt";
-    	String outputFolder = "output1";
+    	String outputFolder = "output2";
 
     	// Create a Java Spark Context
     	SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("exA_spark");
@@ -20,11 +20,14 @@ public class Ex2
     	// Load our input data.
     	JavaRDD<String> input = sc.textFile(inputFile);
 
-		// Transform into integers with the same key of 1
-		JavaPairRDD<Integer,Integer> integers = input.mapToPair(s -> new Tuple2<Integer, Integer>(1, Integer.parseInt(s)));
+		// Transform into a pair of double (the number) and integer (the coefficient) with the same key of 1
+		JavaPairRDD<Integer,Tuple2<Double,Integer>> integers = input.mapToPair(s -> new Tuple2<Integer, Tuple2<Double, Integer>>(1, new Tuple2<Double,Integer>(Double.parseDouble(s),1)));
 
-    	// Reduce by keeping the largest value
-		JavaPairRDD<Integer,Integer> result = integers.reduceByKey((a,b) -> (a < b ? b : a));
+    	// Reduce by summing the numbers and the coefficients
+		JavaPairRDD<Integer,Tuple2<Double, Integer>> sumAndCoeff = integers.reduceByKey((a,b) ->  new Tuple2<Double, Integer>(a._1+b._1, a._2+b._2 )  );
+
+		//we divide the sum by the coefficient
+		JavaRDD<Double>  result = sumAndCoeff.map(s-> s._2._1/s._2._2);
 
 		System.out.println(result.collect());
     	result.saveAsTextFile(outputFolder);
